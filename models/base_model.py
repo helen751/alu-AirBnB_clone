590f21d124e2a708f5f1c85a96ac39211388b0e4
Robert <r.niyonkuru@alustudent.com>
2026-01-09 17:05:41 +0200

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the BaseModel for all of the classes"""
+"""BaseModel for all other classes"""
 
 import uuid
 from datetime import datetime
@@ -8,25 +8,35 @@ import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-      self.id = str(uuid.uuid4())
-      self.created_at = datetime.now()
-      self.updated_at = datetime.now()
-      models.storage.new(self)
+        """constructor method for the class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        """String representation"""
+        """returns the string representation"""
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-       """Updates the updated_at attribute with the current"""
-       self.updated_at = datetime.now()
-       models.storage.save()
+        """updates updated_at attribute with current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-       """Dictionary containing keys or values of __dict__ of the instance"""
-       obj_dict = self.__dict__.copy
-       obj_dict['__class__'] = self.__class__.__name__
-       obj_dict['created_at'] = self.created_at.isoformat()
-       obj_dict['updated_at'] = self.updated_at.isoformat()
-       return obj_dict
+        """returns a dictionary containing all the keys and values
+        of the instance's __dict__."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
