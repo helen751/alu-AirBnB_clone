5d5e441b9b6c41f0afc01279305362274637a273
RobertNiyonkuru98 <r.niyonkuru@alustudent.com>
2026-01-09 08:54:33 -0800

    The console

--- console.py
+++ console.py
@@ -1,7 +1,11 @@
 """Command Line Intepretter"""
 import cmd
 
+
+
+
 class HBNBCommand(cmd.Cmd):
+    """This is the class for the console which is inheriting from cmd.Cmd"""
     prompt = "(hbnb) > "
 
     def do_quit(self, arg):
