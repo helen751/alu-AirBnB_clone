cc539666a93c5d167309b261ec764380fd5bd7ef
Robert <r.niyonkuru@alustudent.com>
2026-01-10 12:45:57 +0200

    Everything up to question number 6

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The __init__ method for models directory"""
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
+
+
+dict_classes = {"BaseModel": BaseModel}
