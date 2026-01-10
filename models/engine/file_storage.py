e345a5d17a68184eb84cdcda81a0cc7c03d3710e
Helen751 <okerekehelenugoeze@gmail.com>
2026-01-10 22:01:00 +0200

    fix engine/file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,55 +1,47 @@
 #!/usr/bin/python3
-
+"""FileStorage engine"""
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 
-class FileStorage():
-    """
-    class that serializes instances to a JSON file and
-    deserializes JSON file to instances
-    """
-    __file_path = 'file.json'
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes them"""
+
+    __file_path = "file.json"
     __objects = {}
 
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User
+    }
+
     def all(self):
-        """Returns the dictionary __objects"""
-        return self.__objects
+        """Returns the dictionary of objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets obj in __objects with key <obj class name>.id"""
+        """Sets obj in __objects"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        from models.base_model import BaseModel
-        """Serializes __objects to the JSON file"""
-        data = {}
-        for key, obj in self.__objects.items():
-            data[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(data, file)
+        """Serializes __objects to JSON file"""
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(
+                {k: v.to_dict() for k, v in FileStorage.__objects.items()},
+                f
+            )
 
     def reload(self):
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            "BaseModel":BaseModel, "User": User, "Place":Place,
-            "State":State, "City":City, "Amenity":Amenity,
-            "Review":Review
-        }
+        """Deserializes JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, obj_dict in data.items():
-                    class_name = obj_dict['__class__']
-                    if class_name in classes:
-                        self.__objects[key] = classes[class_name](**obj_dict)
+            with open(FileStorage.__file_path, "r") as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name = value["__class__"]
+                    cls = self.classes.get(class_name)
+                    if cls:
+                        FileStorage.__objects[key] = cls(**value)
         except FileNotFoundError:
             pass
