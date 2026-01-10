fd50d82519f6abb6dfbdda4cfd75cd1ab6526997
Robert <r.niyonkuru@alustudent.com>
2026-01-10 20:42:01 +0200

    Tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,22 +8,22 @@ class TestUserModel(unittest.TestCase):
     """This is a testcase class that inherits from unittest.TestCase"""
     def setUp(self):
         """setting up the object for test"""
-        self.city_model = City()
+        self.city = City()
 
     def test_instance(self):
-        self.assertIsInstance(self.city_model, City)
+        self.assertIsInstance(self.city, City)
 
     def test_name(self):
-        self.assertIsInstance(self.city_model.name, str)
-        self.assertEqual(self.city_model.name, "")
-        self.city_model.name = "kigali"
-        self.assertEqual(self.city_model.name, "kigali")
+        self.assertIsInstance(self.city.name, str)
+        self.assertEqual(self.city.name, "")
+        self.city.name = "kigali"
+        self.assertEqual(self.city.name, "kigali")
 
     def test_state_id(self):
-        self.assertIsInstance(self.city_model.state_id, str)
-        self.assertEqual(self.city_model.state_id, "")
-        self.city_model.state_id = "1234"
-        self.assertEqual(self.city_model.state_id, "1234")
+        self.assertIsInstance(self.city.state_id, str)
+        self.assertEqual(self.city.state_id, "")
+        self.city.state_id = "1234"
+        self.assertEqual(self.city.state_id, "1234")
 
 if __name__ == "__main__":
     unittest.main()
