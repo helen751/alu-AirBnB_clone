40fa4489f8ec31b99bdd4104cd551e6c4a10e66c
Robert <r.niyonkuru@alustudent.com>
2026-01-10 22:55:25 +0200

    Tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,9 +31,9 @@ class FileStorage():
         from models.base_model import BaseModel
         """Serializes __objects to the JSON file"""
         data = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             data[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
+        with open(FileStorage.__file_path, 'w') as file:
             json.dump(data, file)
 
     def reload(self):
@@ -50,7 +50,3 @@ class FileStorage():
 
 storage = FileStorage()
 storage.reload()
-
-dict_classes = {"BaseModel": BaseModel, "User": User,  "State": State,
-           "City": City, "Amenity": Amenity, "Place": Place,
-           "Review": Review}
