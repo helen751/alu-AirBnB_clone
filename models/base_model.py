71ec7392366680af8aaaae08bb4def33647a0f2f
Helen751 <okerekehelenugoeze@gmail.com>
2026-01-10 17:40:18 +0200

    fix errors on filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
+"""BaseModel for all other classes"""
+
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
-    """BaseModel class"""
-
     def __init__(self, *args, **kwargs):
+        """constructor method for the class"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                if key in ("created_at", "updated_at"):
-                    value = datetime.fromisoformat(value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
+        """returns the string representation"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates updated_at and saves to storage"""
+        """updates updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns dictionary representation"""
+        """returns a dictionary containing all the keys and values
+        of the instance's __dict__."""
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
